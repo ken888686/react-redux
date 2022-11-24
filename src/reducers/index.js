0c85ef535e42ea3e0943def62dd1118df79d0c23
Aaron <ken888686@gmail.com>
2022-11-24 20:32:01 +0900

    create reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,28 @@
+// Reducer
+const songsReducer = () => [
+  {
+    title: "Song A",
+    duration: "00:10",
+  },
+  {
+    title: "Song B",
+    duration: "00:20",
+  },
+  {
+    title: "Song C",
+    duration: "00:30",
+  },
+  {
+    title: "Song D",
+    duration: "00:40",
+  },
+];
+
+// Reducer
+const selectedSongReducer = (action, selectedSong = null) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
